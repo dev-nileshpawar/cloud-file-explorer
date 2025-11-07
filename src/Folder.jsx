eea16c942788bc27bf8a60bbffc0aba64ca7942d
nnnpawar7 <nnnpawar7+git@gmail.com>
2025-11-07 08:45:27 +0530

    code sanitization

--- src/Folder.jsx
+++ src/Folder.jsx
@@ -1,17 +1,21 @@
 import { Fragment, useState } from "react";
 import { FileComponent } from "./File"
 
-export const FolderComponent = ({childs, name})=>{
+export const FolderComponent = ({childs=[], name})=>{
     const [expand, updateExpansion] = useState(false)
     const openCloseFolder = ()=>{
         updateExpansion(!expand);
     }
     return (<div className="folder">
-        <p onClick={()=>openCloseFolder()} style={{cursor:"pointer"}}>{expand ?"┗📂 ":"┗📁 " }{name}</p>
-        {(expand? childs: []).map(({name, type, childs, expand}, i)=>{
-            return (<Fragment key={name}>
-                {type === "folder"?<FolderComponent name={name} childs={childs} /> : <FileComponent name={name}/>}
-            </Fragment>)
-        })}
+        <p onClick={()=>openCloseFolder()} style={{cursor:"pointer"}}>
+            {expand ?"┗📂 ":"┗📁 " }{name}
+        </p>
+        <div className={`folder-children ${expand ? "open" : "closed"}`}>
+            {(expand? childs: []).map(({name, type, childs }, i)=>{
+                return (<Fragment key={name+i}>
+                    {type === "folder"?<FolderComponent name={name} childs={childs} /> : <FileComponent name={name}/>}
+                </Fragment>)
+            })}
+        </div>
     </div>)
 }
